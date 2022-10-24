679610616f5f271367a4d9c0d6059bb398d12fa1
renzorr <benaventer01@gmail.com>
2022-10-24 17:17:18 -0500

    first commit

---
+++ js/profile.js
@@ -0,0 +1,47 @@
+const url = "https://api.github.com/users/Renzorr";
+
+async function loadProfile() {
+  try {
+    const response = await fetch(url);
+    console.log(response);
+
+    if (response.status === 200) {
+      const data = await response.json();
+      render(data);
+    } else {
+      console.log("Something wrong happened");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function render(data) {
+  const profile = `   <div class="profile">
+    <img src="${data.avatar_url}" alt="profile-picture" />
+    <div class="profile-content">
+      <h1 class="profile-login">${data.login}</h1>
+      <span class="profile-name">@${data.name}</span>
+      <p class="profile-date">${data.created_at}</p>
+    </div>
+  </div>
+
+  <div class="profile-details">
+    <div class="details">
+      <p class="type">Repos</p>
+      <span class="type">${data.public_repos}</span>
+    </div>
+    <div class="details">
+      <p class="type">Followers</p>
+      <span class="type">${data.followers}</span>
+    </div>
+    <div class="details">
+      <p class="type">Following</p>
+      <span class="type">${data.following}</span>
+    </div>
+  </div>`;
+
+  document.getElementById("container").innerHTML = profile;
+}
+
+loadProfile();
---
+++ js/repos.js
@@ -0,0 +1,34 @@
+const url = "https://api.github.com/users/Renzorr/repos";
+
+async function loadRepos() {
+  try {
+    const response = await fetch(url);
+    console.log(response);
+
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+      render(data);
+    } else {
+      console.log("Something wrong happened");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function render(data) {
+  let repo = "";
+  data.forEach((repos) => {
+    repo += `   
+      <div class="repos">
+        <h1 class="repo-name">${repos.name}</h1>
+        <a href="${repos.html_url}" class="repo-link" target="_blank" ><i class="fa-sharp fa-solid fa-link"></i>Link</a>
+        <span class="repo-language">${repos.language}</span>
+      </div>`;
+  });
+
+  document.getElementById("container").innerHTML = repo;
+}
+
+loadRepos();
